938d8b5b5abd55e552832d3e617d4df72067db14
ryznlaptop <faizkasman97@gmail.com>
2022-05-16 04:22:07 +0800

    js script for testing gif counter works or not

--- tests/myepicproject.js
+++ tests/myepicproject.js
@@ -1,13 +1,40 @@
 const anchor = require('@project-serum/anchor');
+const { program } = require('@project-serum/anchor/dist/cjs/spl/token');
+const {SystemProgram} = anchor.web3;
 
 const main = async() => {
   console.log("Starting test...")
 
-  anchor.setProvider(anchor.AnchorProvider.env());
+  const provider = anchor.AnchorProvider.env();
+  anchor.setProvider(provider);
+
   const program = anchor.workspace.Myepicproject;
-  const tx = await program.rpc.startStuffOff();
+
+  const baseAccount = anchor.web3.Keypair.generate();
+
+  const tx = await program.rpc.startStuffOff({
+    accounts: {
+      baseAccount: baseAccount.publicKey,
+      user: provider.wallet.publicKey,
+      systemProgram: SystemProgram.programId,
+    },
+    signers: [baseAccount],
+  });
 
   console.log("Your transaction is", tx);
+
+
+  let account = await program.account.baseAccount.fetch(baseAccount.publicKey);
+  console.log('GIF Count', account.totalGifs.toString())
+
+  await program.rpc.addGif({
+    accounts: {
+      baseAccount: baseAccount.publicKey,
+    },
+  });
+
+  account = await program.account.baseAccount.fetch(baseAccount.publicKey);
+  console.log('Your GIF Count now is: ', account.totalGifs.toString())
 }
 
 const runMain = async () => {
